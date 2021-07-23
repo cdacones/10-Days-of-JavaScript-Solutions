fd33546e9ecc2d8177bceb23678046db7b241da5
charmaine <daconesc17@gmail.com>
2021-07-23 00:08:57 -0700

    Delete hello-world.js

--- hello-world.js
+++
@@ -1,9 +0,0 @@
-// Create a function called greeting with one parameter of type string called parameterVariable
-function greeting(parameterVariable) {
-  // Use standard output to print Hello, World! to the console
-  console.log("Hello, World!");
-  // Print the contents of the argument passed to the function
-  console.log(parameterVariable);
-}
-
-
